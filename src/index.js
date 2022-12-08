4d05cae80c6779167c410074ef91408cf587a3ef
Chandrashekhar Singh Mourya <csm8107@gmail.com>
2022-12-08 17:49:03 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express");
+var bodyParser = require("body-parser");
 
 const route = require('./routes/route.js');
 
@@ -8,11 +8,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route);
+app.use("/", route);
 
-app.listen(3000, function() {
-    console.log('Express app running on port ' + 3000)
+app.listen(3000, function () {
+  console.log("Express app running on port " + 3000);
 });
-
 
 // A || B
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('any dummy text')
+router.get("/test-me", function (req, res) {
+  res.send("any dummy text");
 });
 
-
-router.get('/test-you', function(req, res){
-    console.log("I am here")
-    res.send("very important text")
-})
-
+router.get("/test-you", function (req, res) {
+  console.log("I am here");
+  res.send("very important text");
+});
 
 module.exports = router;
